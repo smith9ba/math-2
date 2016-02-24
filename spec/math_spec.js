a9df2d51c65304a5a137b8fcd3250f8c8b1b4ef5
Morgan Benton <morgan.benton@gmail.com>
2016-02-24 11:03:28 -0500

    added tests for add and subtract

--- spec/math_spec.js
+++ spec/math_spec.js
@@ -1,5 +1,13 @@
+// tell the spec where to find our code
+var math = require('../math');
+
 describe("A simple arithmetic program", function() {
+
   it("can add two numbers", function() {
-    expect(add(3,5)).toBe(8);
+    expect(math.add(3,5)).toBe(8);
+  });
+
+  it("can subtract two numbers", function() {
+    expect(math.subtract(3,5)).toBe(-2);
   });
 });
