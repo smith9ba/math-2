8dd5315da4f0f719444d78028f87f5b97fbe54f6
Morgan Benton <morgan.benton@gmail.com>
2016-02-29 11:11:31 -0500

    added code/tests for +-*/

---
+++ arith
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+// get argument values
+var	op   =  process.argv[2], // the operator for the math
+	num1 = +process.argv[3], // the first operand
+	num2 = +process.argv[4], // the second operand
+	math = require('./math');// import our math module
+
+console.log(math[op](num1, num2));
--- spec/math_spec.js
+++ spec/math_spec.js
@@ -5,9 +5,25 @@ describe("A simple arithmetic program", function() {
 
   it("can add two numbers", function() {
     expect(math.add(3,5)).toBe(8);
+    expect(math.add(3.2,5.7)).toBe(8.9);
+    expect(math.add(-3,5)).toBe(2);
+    expect(math.add(3.2,-5.7)).toBe(-2.5);
   });
 
   it("can subtract two numbers", function() {
     expect(math.subtract(3,5)).toBe(-2);
+    expect(math.subtract(3.5,5)).toBe(-1.5);
   });
+
+  it("can multiply two numbers", function() {
+    expect(math.multiply(3,5)).toBe(15);
+    expect(math.multiply(3,0)).toBe(0);
+    expect(math.multiply(3.3,5)).toBe(16.5);
+  });
+
+  it("can divide two numbers", function() {
+    expect(math.divide(15,5)).toBe(3);
+    expect(math.divide(15,0)).toBe("Error! Division by zero."); // ????
+  });
+  
 });
