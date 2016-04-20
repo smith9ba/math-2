34462cb77bed58d251a99f6615f41b63a189bfd5
smith9ba <smith9ba@dukes.jmu.edu>
2016-04-20 15:47:48 -0400

    Added files via upload

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# Math: A JavaScript/Node Arithmetic Program
+#Math: A JavaScript/Node Arithmetic Program
 
-This is a simple JavaScript/NodeJS program that does basic arithmetic and demonstrates test-driven development (TDD).
+This is a simple JavaScript/NodeJS program that does basic arithemtic and demonstrates test-driven development (TDD).
+
--- spec/math_spec.js
+++ spec/math_spec.js
@@ -1,29 +1,22 @@
-// tell the spec where to find our code
+// tell jasmine where to find our code
 var math = require('../math');
 
-describe("A simple arithmetic program", function() {
+describe("A program that does basic arithmetic", function() {
 
-  it("can add two numbers", function() {
+  it("can add two numbers together", function() {
     expect(math.add(3,5)).toBe(8);
-    expect(math.add(3.2,5.7)).toBe(8.9);
-    expect(math.add(-3,5)).toBe(2);
-    expect(math.add(3.2,-5.7)).toBe(-2.5);
   });
 
   it("can subtract two numbers", function() {
     expect(math.subtract(3,5)).toBe(-2);
-    expect(math.subtract(3.5,5)).toBe(-1.5);
   });
 
-  it("can multiply two numbers", function() {
-    expect(math.multiply(3,5)).toBe(15);
-    expect(math.multiply(3,0)).toBe(0);
-    expect(math.multiply(3.3,5)).toBe(16.5);
+  it("can multipy two numbers together", function() {
+    expect(math.multipy(3,5)).toBe(15);
   });
 
   it("can divide two numbers", function() {
     expect(math.divide(15,5)).toBe(3);
-    expect(math.divide(15,0)).toBe("Error! Division by zero."); // ????
   });
-  
+
 });
